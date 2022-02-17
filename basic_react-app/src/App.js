ed2647a3cd0b6e1474aaa9999eaef14ff0613c6d
Nishank Yadav <Nishank.Yadav@kyndryl.com>
2022-02-17 21:19:50 +0530

    React Basics

---
+++ basic_react-app/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItems from "./components/ExpenseItems";
+
+function App() {
+  return (
+    <div>
+      <h2>Let's get started</h2>
+      <h3>It's amazing</h3>
+      <ExpenseItems />
+    </div>
+  )
+}
+
+export default App;
---
+++ basic_react-app/src/components/ExpenseItems.js
@@ -0,0 +1,13 @@
+function ExpenseItems() {
+    return (
+        <div>
+            <div>Feb 17</div>
+            <div>
+                <h2>Medical Bills</h2>
+                <div>$90</div>
+            </div>
+        </div>
+    );
+}
+
+export default ExpenseItems;
